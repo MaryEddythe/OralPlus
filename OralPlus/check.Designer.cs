429544d3278fead4f088a897e8300847c963c859
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-02 14:07:25 +0800

    fix check

--- OralPlus/check.Designer.cs
+++ OralPlus/check.Designer.cs
@@ -29,12 +29,313 @@ namespace OralPlus
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_contact = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_add = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radio_xx = new System.Windows.Forms.RadioButton();
+            this.radio_female = new System.Windows.Forms.RadioButton();
+            this.radio_male = new System.Windows.Forms.RadioButton();
+            this.date_dob = new System.Windows.Forms.DateTimePicker();
+            this.txt_fname = new System.Windows.Forms.TextBox();
+            this.txt_lname = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.btn_check = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(660, 495);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 26);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(872, 574);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 26);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Contact Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(660, 574);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 26);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Email";
+            // 
+            // txt_contact
+            // 
+            this.txt_contact.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_contact.Location = new System.Drawing.Point(877, 603);
+            this.txt_contact.Multiline = true;
+            this.txt_contact.Name = "txt_contact";
+            this.txt_contact.Size = new System.Drawing.Size(189, 36);
+            this.txt_contact.TabIndex = 28;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_email.Location = new System.Drawing.Point(660, 603);
+            this.txt_email.Multiline = true;
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(189, 36);
+            this.txt_email.TabIndex = 27;
+            // 
+            // txt_add
+            // 
+            this.txt_add.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_add.Location = new System.Drawing.Point(660, 524);
+            this.txt_add.Multiline = true;
+            this.txt_add.Name = "txt_add";
+            this.txt_add.Size = new System.Drawing.Size(406, 36);
+            this.txt_add.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(660, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 26);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Date of Birth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(872, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 26);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(660, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 26);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Last Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(660, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 26);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Sex";
+            // 
+            // radio_xx
+            // 
+            this.radio_xx.AutoSize = true;
+            this.radio_xx.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_xx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radio_xx.Location = new System.Drawing.Point(901, 381);
+            this.radio_xx.Name = "radio_xx";
+            this.radio_xx.Size = new System.Drawing.Size(147, 29);
+            this.radio_xx.TabIndex = 21;
+            this.radio_xx.TabStop = true;
+            this.radio_xx.Text = "Prefer not to say";
+            this.radio_xx.UseVisualStyleBackColor = true;
+            // 
+            // radio_female
+            // 
+            this.radio_female.AutoSize = true;
+            this.radio_female.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_female.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radio_female.Location = new System.Drawing.Point(781, 381);
+            this.radio_female.Name = "radio_female";
+            this.radio_female.Size = new System.Drawing.Size(84, 29);
+            this.radio_female.TabIndex = 20;
+            this.radio_female.TabStop = true;
+            this.radio_female.Text = "Female";
+            this.radio_female.UseVisualStyleBackColor = true;
+            // 
+            // radio_male
+            // 
+            this.radio_male.AutoSize = true;
+            this.radio_male.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_male.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radio_male.Location = new System.Drawing.Point(660, 381);
+            this.radio_male.Name = "radio_male";
+            this.radio_male.Size = new System.Drawing.Size(66, 29);
+            this.radio_male.TabIndex = 19;
+            this.radio_male.TabStop = true;
+            this.radio_male.Text = "Male";
+            this.radio_male.UseVisualStyleBackColor = true;
+            // 
+            // date_dob
+            // 
+            this.date_dob.Font = new System.Drawing.Font("Poppins SemiBold", 8F, System.Drawing.FontStyle.Bold);
+            this.date_dob.Location = new System.Drawing.Point(660, 454);
+            this.date_dob.Name = "date_dob";
+            this.date_dob.Size = new System.Drawing.Size(406, 27);
+            this.date_dob.TabIndex = 18;
+            // 
+            // txt_fname
+            // 
+            this.txt_fname.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_fname.Location = new System.Drawing.Point(877, 302);
+            this.txt_fname.Multiline = true;
+            this.txt_fname.Name = "txt_fname";
+            this.txt_fname.Size = new System.Drawing.Size(189, 36);
+            this.txt_fname.TabIndex = 17;
+            // 
+            // txt_lname
+            // 
+            this.txt_lname.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_lname.Location = new System.Drawing.Point(660, 302);
+            this.txt_lname.Multiline = true;
+            this.txt_lname.Name = "txt_lname";
+            this.txt_lname.Size = new System.Drawing.Size(189, 36);
+            this.txt_lname.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(91, 189);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 450);
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(91, 159);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(121, 24);
+            this.cboDevice.TabIndex = 33;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(143)))), ((int)(((byte)(213)))));
+            this.btn_scan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_scan.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.btn_scan.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_scan.Location = new System.Drawing.Point(180, 663);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(267, 38);
+            this.btn_scan.TabIndex = 34;
+            this.btn_scan.Text = "SCAN QR CODE";
+            this.btn_scan.UseVisualStyleBackColor = false;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // btn_check
+            // 
+            this.btn_check.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(139)))), ((int)(((byte)(0)))));
+            this.btn_check.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_check.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.btn_check.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_check.Location = new System.Drawing.Point(746, 666);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(267, 38);
+            this.btn_check.TabIndex = 35;
+            this.btn_check.Text = "Check in";
+            this.btn_check.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Poppins SemiBold", 16F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(724, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(308, 48);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Personal Information";
+            // 
+            // check
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1228, 840);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_check);
+            this.Controls.Add(this.btn_scan);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_contact);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radio_xx);
+            this.Controls.Add(this.radio_female);
+            this.Controls.Add(this.radio_male);
+            this.Controls.Add(this.date_dob);
+            this.Controls.Add(this.txt_fname);
+            this.Controls.Add(this.txt_lname);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "check";
             this.Text = "check";
+            this.Load += new System.EventHandler(this.check_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_contact;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_add;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radio_xx;
+        private System.Windows.Forms.RadioButton radio_female;
+        private System.Windows.Forms.RadioButton radio_male;
+        private System.Windows.Forms.DateTimePicker date_dob;
+        private System.Windows.Forms.TextBox txt_fname;
+        private System.Windows.Forms.TextBox txt_lname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Button btn_scan;
+        private System.Windows.Forms.Button btn_check;
+        private System.Windows.Forms.Label label8;
     }
 }
--- OralPlus/check.cs
+++ OralPlus/check.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace OralPlus
 {
@@ -15,6 +17,37 @@ namespace OralPlus
         public check()
         {
             InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+
+        private void check_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboDevice.Items.Add(filterInfo.Name);
+            cboDevice.SelectedIndex = 0;
+        }
+
+        private void btn_scan_Click(object sender, EventArgs e)
+        {
+            if (cboDevice.SelectedIndex >= 0 && cboDevice.SelectedIndex < filterInfoCollection.Count)
+            {
+                FilterInfo selectedFilter = filterInfoCollection[cboDevice.SelectedIndex];
+                captureDevice = new VideoCaptureDevice(selectedFilter.MonikerString);
+                captureDevice.NewFrame += CaptureDevice_NewFrame;
+                captureDevice.Start();
+            }
+            else
+            {
+               
+                MessageBox.Show("Please select a valid device.");
+            }
+        }
+
+        private void CaptureDevice_NewFrame(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
