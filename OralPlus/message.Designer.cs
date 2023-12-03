bd928b1f5ec5e42534a36546ced1026fe09b8884
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-03 10:16:16 +0800

    EDIT MESSAGE

--- OralPlus/message.Designer.cs
+++ OralPlus/message.Designer.cs
@@ -35,8 +35,10 @@ namespace OralPlus
             this.label4 = new System.Windows.Forms.Label();
             this.txt_message = new System.Windows.Forms.TextBox();
             this.btn_send = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.txt_name = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -44,16 +46,16 @@ namespace OralPlus
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(172, 248);
+            this.label2.Location = new System.Drawing.Point(68, 241);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 26);
+            this.label2.Size = new System.Drawing.Size(60, 26);
             this.label2.TabIndex = 13;
-            this.label2.Text = "To:";
+            this.label2.Text = "Phone";
             // 
             // txt_contact
             // 
             this.txt_contact.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
-            this.txt_contact.Location = new System.Drawing.Point(226, 238);
+            this.txt_contact.Location = new System.Drawing.Point(134, 238);
             this.txt_contact.Multiline = true;
             this.txt_contact.Name = "txt_contact";
             this.txt_contact.Size = new System.Drawing.Size(398, 36);
@@ -64,18 +66,18 @@ namespace OralPlus
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(232, 209);
+            this.label1.Location = new System.Drawing.Point(68, 294);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 26);
+            this.label1.Size = new System.Drawing.Size(58, 26);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Last Name";
+            this.label1.Text = "Name";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(124, 390);
+            this.label4.Location = new System.Drawing.Point(32, 390);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 26);
             this.label4.TabIndex = 29;
@@ -84,7 +86,7 @@ namespace OralPlus
             // txt_message
             // 
             this.txt_message.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
-            this.txt_message.Location = new System.Drawing.Point(226, 390);
+            this.txt_message.Location = new System.Drawing.Point(134, 390);
             this.txt_message.Multiline = true;
             this.txt_message.Name = "txt_message";
             this.txt_message.Size = new System.Drawing.Size(398, 173);
@@ -96,7 +98,7 @@ namespace OralPlus
             this.btn_send.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_send.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_send.ForeColor = System.Drawing.SystemColors.Control;
-            this.btn_send.Location = new System.Drawing.Point(457, 597);
+            this.btn_send.Location = new System.Drawing.Point(365, 597);
             this.btn_send.Name = "btn_send";
             this.btn_send.Size = new System.Drawing.Size(167, 38);
             this.btn_send.TabIndex = 31;
@@ -104,43 +106,54 @@ namespace OralPlus
             this.btn_send.UseVisualStyleBackColor = false;
             this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(172, 301);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 26);
-            this.label3.TabIndex = 33;
-            this.label3.Text = "To:";
-            // 
             // txt_name
             // 
             this.txt_name.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
-            this.txt_name.Location = new System.Drawing.Point(226, 291);
+            this.txt_name.Location = new System.Drawing.Point(134, 291);
             this.txt_name.Multiline = true;
             this.txt_name.Name = "txt_name";
             this.txt_name.Size = new System.Drawing.Size(398, 36);
             this.txt_name.TabIndex = 32;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_message);
+            this.panel1.Controls.Add(this.txt_contact);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btn_send);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 840);
+            this.panel1.TabIndex = 34;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 20F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(146, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(345, 60);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "SMS NOTIFICATION";
             // 
             // message
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_name);
-            this.Controls.Add(this.btn_send);
-            this.Controls.Add(this.txt_message);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txt_contact);
+            this.Controls.Add(this.panel1);
             this.Name = "message";
             this.Size = new System.Drawing.Size(1228, 840);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -152,7 +165,8 @@ namespace OralPlus
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txt_message;
         private System.Windows.Forms.Button btn_send;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
     }
 }
