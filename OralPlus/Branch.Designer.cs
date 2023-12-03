758f5853774a44f58e50e2987a65c32459bea087
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-04 01:30:52 +0800

    branch ui

--- OralPlus/Branch.Designer.cs
+++ OralPlus/Branch.Designer.cs
@@ -29,19 +29,22 @@ namespace OralPlus
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Branch));
             this.btn_cebu = new System.Windows.Forms.Button();
             this.btn_ilo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_cebu
             // 
-            this.btn_cebu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(178)))));
+            this.btn_cebu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(145)))), ((int)(((byte)(33)))));
             this.btn_cebu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cebu.Font = new System.Drawing.Font("Poppins Medium", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cebu.Font = new System.Drawing.Font("Poppins SemiBold", 12F);
             this.btn_cebu.ForeColor = System.Drawing.SystemColors.Control;
-            this.btn_cebu.Location = new System.Drawing.Point(679, 470);
+            this.btn_cebu.Location = new System.Drawing.Point(744, 480);
             this.btn_cebu.Name = "btn_cebu";
-            this.btn_cebu.Size = new System.Drawing.Size(167, 38);
+            this.btn_cebu.Size = new System.Drawing.Size(420, 48);
             this.btn_cebu.TabIndex = 10;
             this.btn_cebu.Text = "Cebu City ";
             this.btn_cebu.UseVisualStyleBackColor = false;
@@ -51,27 +54,39 @@ namespace OralPlus
             // 
             this.btn_ilo.BackColor = System.Drawing.Color.SteelBlue;
             this.btn_ilo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_ilo.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.btn_ilo.Font = new System.Drawing.Font("Poppins SemiBold", 12F);
             this.btn_ilo.ForeColor = System.Drawing.SystemColors.Control;
-            this.btn_ilo.Location = new System.Drawing.Point(180, 470);
+            this.btn_ilo.Location = new System.Drawing.Point(85, 482);
             this.btn_ilo.Name = "btn_ilo";
-            this.btn_ilo.Size = new System.Drawing.Size(167, 38);
+            this.btn_ilo.Size = new System.Drawing.Size(420, 48);
             this.btn_ilo.TabIndex = 9;
             this.btn_ilo.Text = "Iloilo CIty";
             this.btn_ilo.UseVisualStyleBackColor = false;
             this.btn_ilo.Click += new System.EventHandler(this.btn_ilo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1279, 708);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
             // 
             // Branch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(1022, 620);
+            this.ClientSize = new System.Drawing.Size(1276, 708);
             this.Controls.Add(this.btn_cebu);
             this.Controls.Add(this.btn_ilo);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Branch";
             this.Text = "Branch";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -80,5 +95,6 @@ namespace OralPlus
 
         private System.Windows.Forms.Button btn_cebu;
         private System.Windows.Forms.Button btn_ilo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
