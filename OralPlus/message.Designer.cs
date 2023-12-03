717680660bebfb2fe710cfa084532cbe6e1753b0
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-03 09:50:35 +0800

    send sms

--- OralPlus/message.Designer.cs
+++ OralPlus/message.Designer.cs
@@ -29,19 +29,130 @@ namespace OralPlus
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_contact = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(172, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 26);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "To:";
+            // 
+            // txt_contact
+            // 
+            this.txt_contact.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_contact.Location = new System.Drawing.Point(226, 238);
+            this.txt_contact.Multiline = true;
+            this.txt_contact.Name = "txt_contact";
+            this.txt_contact.Size = new System.Drawing.Size(398, 36);
+            this.txt_contact.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(232, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(124, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 26);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Message:";
+            // 
+            // txt_message
+            // 
+            this.txt_message.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_message.Location = new System.Drawing.Point(226, 390);
+            this.txt_message.Multiline = true;
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(398, 173);
+            this.txt_message.TabIndex = 30;
+            // 
+            // btn_send
+            // 
+            this.btn_send.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(139)))), ((int)(((byte)(0)))));
+            this.btn_send.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_send.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_send.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_send.Location = new System.Drawing.Point(457, 597);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(167, 38);
+            this.btn_send.TabIndex = 31;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = false;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(172, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 26);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "To:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Poppins SemiBold", 9F);
+            this.txt_name.Location = new System.Drawing.Point(226, 291);
+            this.txt_name.Multiline = true;
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(398, 36);
+            this.txt_name.TabIndex = 32;
             // 
             // message
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_contact);
             this.Name = "message";
             this.Size = new System.Drawing.Size(1228, 840);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_contact;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name;
     }
 }
--- OralPlus/message.cs
+++ OralPlus/message.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Collections.Specialized;
+using System.IO;
 
 namespace OralPlus
 {
@@ -16,5 +19,49 @@ namespace OralPlus
         {
             InitializeComponent();
         }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+			String result;
+			string apiKey = "NjY3MTZmN2EzOTM2NmY3Njc1NDk0OTRhNmU3Nzc2NGU=";
+			string numbers = txt_contact.Text;
+
+			string send = "OralPlus+ Dental Clinic";
+			string name = txt_name.Text;
+			string message = txt_message.Text;
+
+			String url = "https://api.txtlocal.com/send/?apikey=" + apiKey + "&numbers=" + numbers + "&message=" + message + "&sender=" + sender;
+			//refer to parameters to complete correct url string
+
+			StreamWriter myWriter = null;
+			HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+
+			objRequest.Method = "POST";
+			objRequest.ContentLength = Encoding.UTF8.GetByteCount(url);
+			objRequest.ContentType = "application/x-www-form-urlencoded";
+			try
+			{
+				myWriter = new StreamWriter(objRequest.GetRequestStream());
+				myWriter.Write(url);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				myWriter.Close();
+			}
+
+			HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+			using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+			{
+				result = sr.ReadToEnd();
+				// Close and clean up the StreamReader
+				sr.Close();
+			}
+			MessageBox.Show("Appointment reminder sent successfully!");
+		}
+
     }
 }
