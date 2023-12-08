ffc044d2ed46ff303a802cf53a858aac3551ab91
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-09 01:24:40 +0800

    x

--- OralPlus/AddApp.Designer.cs
+++ OralPlus/AddApp.Designer.cs
@@ -32,16 +32,7 @@ namespace OralPlus
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddApp));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label10 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.label19 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label18 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label17 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
-            this.label16 = new System.Windows.Forms.Label();
             this.btn_exit = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -59,9 +50,18 @@ namespace OralPlus
             this.txt_fname = new System.Windows.Forms.TextBox();
             this.txt_lname = new System.Windows.Forms.TextBox();
             this.btn_next = new System.Windows.Forms.Button();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_exit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -96,17 +96,6 @@ namespace OralPlus
             this.label10.TabIndex = 52;
             this.label10.Text = "oralplus@gmail.com";
             // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label14.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label14.Location = new System.Drawing.Point(21, 399);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(365, 25);
-            this.label14.TabIndex = 54;
-            this.label14.Text = "Experience top-notch oral health services with our\r\n";
-            // 
             // label11
             // 
             this.label11.AutoSize = true;
@@ -118,95 +107,6 @@ namespace OralPlus
             this.label11.Size = new System.Drawing.Size(129, 26);
             this.label11.TabIndex = 50;
             this.label11.Text = "+639293871698";
-            // 
-            // label19
-            // 
-            this.label19.AutoSize = true;
-            this.label19.BackColor = System.Drawing.Color.Transparent;
-            this.label19.Font = new System.Drawing.Font("Poppins ExtraBold", 17F, System.Drawing.FontStyle.Bold);
-            this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(68)))));
-            this.label19.Location = new System.Drawing.Point(67, 278);
-            this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(245, 51);
-            this.label19.TabIndex = 47;
-            this.label19.Text = "Schedule your ";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label12.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label12.Location = new System.Drawing.Point(61, 449);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(236, 25);
-            this.label12.TabIndex = 56;
-            this.label12.Text = "maintaining your healthy smile.";
-            // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.BackColor = System.Drawing.Color.Transparent;
-            this.label18.Font = new System.Drawing.Font("Poppins ExtraBold", 17F, System.Drawing.FontStyle.Bold);
-            this.label18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(68)))));
-            this.label18.Location = new System.Drawing.Point(36, 313);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(304, 51);
-            this.label18.TabIndex = 48;
-            this.label18.Text = "appointment now!";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label13.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label13.Location = new System.Drawing.Point(21, 424);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(349, 25);
-            this.label13.TabIndex = 55;
-            this.label13.Text = "team! Our friendly team is ready to assist you in";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(145, 165);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.TabIndex = 49;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label17
-            // 
-            this.label17.AutoSize = true;
-            this.label17.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label17.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label17.Location = new System.Drawing.Point(12, 377);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(384, 25);
-            this.label17.TabIndex = 46;
-            this.label17.Text = "Schedule your next dental appointment hassle-free! ";
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label15.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label15.Location = new System.Drawing.Point(21, 505);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(358, 25);
-            this.label15.TabIndex = 53;
-            this.label15.Text = "Follow the steps to setup an appointment with us";
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Font = new System.Drawing.Font("Poppins Medium", 8F);
-            this.label16.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label16.Location = new System.Drawing.Point(41, 530);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(280, 25);
-            this.label16.TabIndex = 51;
-            this.label16.Text = "with us to get the best dental solution.";
             // 
             // btn_exit
             // 
@@ -400,6 +300,106 @@ namespace OralPlus
             this.btn_next.UseVisualStyleBackColor = false;
             this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
             // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label16.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label16.Location = new System.Drawing.Point(41, 530);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(280, 25);
+            this.label16.TabIndex = 51;
+            this.label16.Text = "with us to get the best dental solution.";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label15.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label15.Location = new System.Drawing.Point(21, 505);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(358, 25);
+            this.label15.TabIndex = 53;
+            this.label15.Text = "Follow the steps to setup an appointment with us";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label17.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label17.Location = new System.Drawing.Point(12, 377);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(384, 25);
+            this.label17.TabIndex = 46;
+            this.label17.Text = "Schedule your next dental appointment hassle-free! ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(145, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label13.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label13.Location = new System.Drawing.Point(21, 424);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(349, 25);
+            this.label13.TabIndex = 55;
+            this.label13.Text = "team! Our friendly team is ready to assist you in";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Transparent;
+            this.label18.Font = new System.Drawing.Font("Poppins ExtraBold", 17F, System.Drawing.FontStyle.Bold);
+            this.label18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(68)))));
+            this.label18.Location = new System.Drawing.Point(36, 313);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(304, 51);
+            this.label18.TabIndex = 48;
+            this.label18.Text = "appointment now!";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label12.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label12.Location = new System.Drawing.Point(61, 449);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(236, 25);
+            this.label12.TabIndex = 56;
+            this.label12.Text = "maintaining your healthy smile.";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.Color.Transparent;
+            this.label19.Font = new System.Drawing.Font("Poppins ExtraBold", 17F, System.Drawing.FontStyle.Bold);
+            this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(68)))));
+            this.label19.Location = new System.Drawing.Point(67, 278);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(245, 51);
+            this.label19.TabIndex = 47;
+            this.label19.Text = "Schedule your ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Poppins Medium", 8F);
+            this.label14.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label14.Location = new System.Drawing.Point(21, 399);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(365, 25);
+            this.label14.TabIndex = 54;
+            this.label14.Text = "Experience top-notch oral health services with our\r\n";
+            // 
             // AddApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -430,8 +430,8 @@ namespace OralPlus
             this.Load += new System.EventHandler(this.AddApp_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_exit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -441,16 +441,7 @@ namespace OralPlus
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox btn_exit;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label19;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label18;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_contact;
@@ -467,5 +458,14 @@ namespace OralPlus
         private System.Windows.Forms.TextBox txt_fname;
         private System.Windows.Forms.TextBox txt_lname;
         private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
     }
 }
