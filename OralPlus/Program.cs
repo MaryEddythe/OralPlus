7078d0aba8f3a0be4cc9bd22d22105cabfad9cf6
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-09 01:20:30 +0800

    vsdvs

--- OralPlus/Program.cs
+++ OralPlus/Program.cs
@@ -16,7 +16,7 @@ namespace OralPlus
         {
             //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form1());
         }
     }
 }
