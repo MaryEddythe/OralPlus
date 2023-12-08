5889b2782cb3ce999e2e9be60385fe405ef695c9
MaryEddythe <sornito.maryeddythe@gmail.com>
2023-12-08 23:33:17 +0800

    wwww

--- OralPlus/Program.cs
+++ OralPlus/Program.cs
@@ -16,7 +16,7 @@ namespace OralPlus
         {
             //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Splash());
+            Application.Run(new Branch());
         }
     }
 }
